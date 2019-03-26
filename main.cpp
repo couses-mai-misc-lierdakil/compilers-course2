efe2247b338ac8185f0888c902e1fceaa69a5f53
Nikolay Yakimov <root@livid.pp.ru>
2019-03-26 22:15:02 +0300

    Add read from file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "driver.h"
 #include "parser.tab.hh"
+#include <fstream>
 #include <iostream>
 #include <sstream>
 
-int main() {
+int main(int argc, const char *argv[]) {
   std::ios::sync_with_stdio(false);
   auto loc = std::locale(std::locale(""), "C", std::locale::numeric);
   std::locale::global(loc);
@@ -12,18 +13,33 @@ int main() {
   std::wcerr.imbue(std::locale());
   std::wstring line;
   DriverState st;
-  while (!std::getline(std::wcin, line).eof()) {
-    Driver drv(st, line);
-    drv.result = 0;
-    yy::parser parser(drv);
-    // parser.set_debug_level(true);
-    auto res = parser.parse();
-    if (res != 0) {
-      std::wcerr << L"Parser returned error " << res << std::endl;
-    } else {
-      std::wcerr << "Input: " << line << std::endl;
-      std::wcout << "Result is: " << drv.result << std::endl;
+  std::vector<const char *> args(argv + 1, argv + argc);
+  std::wistream *in = &std::wcin;
+  bool fileInput = false;
+  if (args.size() == 1) {
+    in = new std::wifstream(args[0]);
+    fileInput = true;
+  }
+  try {
+    while (!std::getline(*in, line).eof()) {
+      if (fileInput)
+        std::wcerr << "Input: " << line << std::endl;
+      Driver drv(st, line);
+      yy::parser parser(drv);
+      // parser.set_debug_level(true);
+      auto res = parser.parse();
+      if (res != 0) {
+        std::wcerr << L"Parser returned error " << res << std::endl;
+      } else {
+        if (drv.result)
+          std::wcout << "Result is: " << drv.result.value() << std::endl;
+      }
     }
+  } catch (std::exception &e) {
+    std::wcerr << e.what() << std::endl;
+  }
+  if (fileInput) {
+    delete in;
   }
   return 0;
 }
